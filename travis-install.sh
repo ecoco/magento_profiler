514ab0421fb1ed23703ac153a8e0fde7cb205f64
j.krapp <jk@ecocode.de>
2017-09-05 23:22:40 +0200

    [#14] - travis

--- travis-install.sh
+++ travis-install.sh
@@ -5,11 +5,11 @@ set -ev
 if [ $MAGENTO_VERSION == "magento-mirror-1.7.0.2" ]
 then
     sudo service mysql stop
-    docker run --name mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:5.5
+    docker run --name mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes  -e MYSQL_ROOT_PASSWORD="" -d mysql:5.5
 fi
 
 #Clean mysql database
-mysql -e "DROP DATABASE IF EXISTS magento_test; CREATE DATABASE IF NOT EXISTS magento_test;" -uroot
+mysql -h 127.0.0.1 -e "DROP DATABASE IF EXISTS magento_test; CREATE DATABASE IF NOT EXISTS magento_test;" -uroot
 
 cd $TRAVIS_BUILD_DIR/build
 

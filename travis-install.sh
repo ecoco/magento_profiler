1f93962e403647c11bb77f666192983146e0aa80
Justus Krapp <jk@ecocode.de>
2018-02-19 23:42:24 +0100

    [#14] - travis

--- travis-install.sh
+++ travis-install.sh
@@ -12,8 +12,8 @@ cat <<EOF > ~/.n98-magerun.yaml
 commands:
   N98\Magento\Command\Installer\InstallCommand:
     magento-packages:
-      - name: magento-mirror-1.7.0.2
-        version: 1.7.0.2
+      - name: magento-mirror-1.7.0.2-fix
+        version: 1.7.0.2-fix
         dist:
           url: https://github.com/ecoco/magento-mirror/archive/1.7.0.2-fix.zip
           type: zip
